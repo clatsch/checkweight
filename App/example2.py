ed694535eaf0759a5db4769e42d598d50339be61
Cla Tschenett <cla@aimara.ch>
2023-01-12 18:39:56 +0100

    example2 added

---
+++ App/example2.py
@@ -0,0 +1,47 @@
+#! /usr/bin/python2
+
+import time
+import sys
+
+EMULATE_HX711=False
+
+referenceUnit = -1
+
+if not EMULATE_HX711:
+    import RPi.GPIO as GPIO
+    from hx711 import HX711
+else:
+    from emulated_hx711 import HX711
+
+def cleanAndExit():
+    print("Cleaning...")
+
+    if not EMULATE_HX711:
+        GPIO.cleanup()
+
+    print("Bye!")
+    sys.exit()
+
+hx = HX711(25, 2)
+hx.set_reference_unit(referenceUnit)
+hx.reset()
+
+hx.tare()
+
+print("Tare done! Add weight now...")
+
+def func():
+    while True:
+        try:
+            val = hx.get_weight(5)
+            yield val
+            hx.power_down()
+            hx.power_up()
+            time.sleep(0.1)
+
+        except (KeyboardInterrupt, SystemExit):
+            cleanAndExit()
+
+function = func()
+for i in function:
+    print(i)
--- README.md
+++ README.md
@@ -24,7 +24,7 @@ Great explanation of strain gauges: https://www.allaboutcircuits.com/textbook/di
 
 The python class HX711 can be installed with the following command:
    
-   `pip3 install 'git+https://github.com/bytedisciple/HX711.git#egg=HX711&subdirectory=HX711_Python3'`
+   `sudo pip3 install 'git+https://github.com/bytedisciple/HX711.git#egg=HX711&subdirectory=HX711_Python3'`
    
 Once installed instantiate a HX711 object with the following:
 
