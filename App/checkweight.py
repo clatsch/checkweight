02055f5d7d0176d622df7906e614d96a192a42b5
Cla Tschenett <cla@aimara.ch>
2023-01-17 20:15:26 +0100

    mqtt added

--- App/checkweight.py
+++ App/checkweight.py
@@ -1,6 +1,52 @@
 #!/usr/bin/env python3
 import RPi.GPIO as GPIO  # import GPIO
 from hx711 import HX711  # import the class HX711
+import random
+import time
+from paho.mqtt import client as mqtt_client
+
+broker = 'broker.emqx.io'
+port = 1883
+topic = "python/checkweight"
+# generate client ID with pub prefix randomly
+client_id = f'python-mqtt-{random.randint(0, 1000)}'
+# username = 'emqx'
+# password = 'public'
+
+def publish(client):
+    msg_count = 0
+    while True:
+        time.sleep(1)
+        msg = f"messages: {msg_count}"
+        result = client.publish(topic, msg)
+        # result: [0, 1]
+        status = result[0]
+        if status == 0:
+            print(f"Send `{msg}` to topic `{topic}`")
+        else:
+            print(f"Failed to send message to topic {topic}")
+        msg_count += 1
+
+if __name__ == '__main__':
+    run()
+
+def run():
+    client = connect_mqtt()
+    client.loop_start()
+    publish(client)
+
+def connect_mqtt():
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
+
+    client = mqtt_client.Client(client_id)
+    client.username_pw_set(username, password)
+    client.on_connect = on_connect
+    client.connect(broker, port)
+    return client
 
 try:
     GPIO.setmode(GPIO.BCM)  # set GPIO pin mode to BCM numbering
@@ -42,7 +88,7 @@ try:
         # scale ratio. Without arguments 'channel' and 'gain_A' it sets
         # the ratio for current channel and gain.
         ratio = reading / value  # calculate the ratio for channel A and gain 128
-        print(reading, value)
+        print(ratio, reading, value)
         hx.set_scale_ratio(ratio)  # set ratio for current channel
         print('Ratio is set.')
     else:
