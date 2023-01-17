835471425be76ec66e7d6d4f07d183d86be32fef
Cla Tschenett <cla@aimara.ch>
2023-01-17 20:19:52 +0100

    mqtt added

--- App/checkweight.py
+++ App/checkweight.py
@@ -3,15 +3,31 @@ import RPi.GPIO as GPIO  # import GPIO
 from hx711 import HX711  # import the class HX711
 import random
 import time
+
 from paho.mqtt import client as mqtt_client
 
-broker = 'licalhost'
+
+broker = 'localhost'
 port = 1883
 topic = "python/checkweight"
 # generate client ID with pub prefix randomly
 client_id = f'python-mqtt-{random.randint(0, 1000)}'
 # username = 'emqx'
 # password = 'public'
+
+def connect_mqtt():
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
+
+    client = mqtt_client.Client(client_id)
+    # client.username_pw_set(username, password)
+    client.on_connect = on_connect
+    client.connect(broker, port)
+    return client
+
 
 def publish(client):
     msg_count = 0
@@ -27,31 +43,15 @@ def publish(client):
             print(f"Failed to send message to topic {topic}")
         msg_count += 1
 
+
 def run():
     client = connect_mqtt()
     client.loop_start()
     publish(client)
+
 
 if __name__ == '__main__':
     run()
-
-def run():
-    client = connect_mqtt()
-    client.loop_start()
-    publish(client)
-
-def connect_mqtt():
-    def on_connect(client, userdata, flags, rc):
-        if rc == 0:
-            print("Connected to MQTT Broker!")
-        else:
-            print("Failed to connect, return code %d\n", rc)
-
-    client = mqtt_client.Client(client_id)
-    client.username_pw_set(username, password)
-    client.on_connect = on_connect
-    client.connect(broker, port)
-    return client
 
 try:
     GPIO.setmode(GPIO.BCM)  # set GPIO pin mode to BCM numbering
