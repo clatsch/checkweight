69c126890a0ce7e846d98cb53f9d9fd7f903a165
Cla Tschenett <cla@aimara.ch>
2023-01-17 20:35:43 +0100

    mqtt added

--- App/checkweight.py
+++ App/checkweight.py
@@ -29,31 +29,31 @@ def connect_mqtt():
     return client
 
 
-def publish(client):
-    msg_count = 0
-
-
-    while True:
-        time.sleep(1)
-        msg = f"messages: {msg_count}"
-        result = client.publish(topic, msg)
-        # result: [0, 1]
-        status = result[0]
-        if status == 0:
-            print(f"Send `{msg}` to topic `{topic}`")
-        else:
-            print(f"Failed to send message to topic {topic}")
-        msg_count += 1
-
-
-def run():
-    client = connect_mqtt()
-    # client.loop_start()
-    publish(client)
-
-
-if __name__ == '__main__':
-    run()
+# def publish(client):
+#     msg_count = 0
+#
+#
+#     while True:
+#         time.sleep(1)
+#         msg = f"messages: {msg_count}"
+#         result = client.publish(topic, msg)
+#         # result: [0, 1]
+#         status = result[0]
+#         if status == 0:
+#             print(f"Send `{msg}` to topic `{topic}`")
+#         else:
+#             print(f"Failed to send message to topic {topic}")
+#         msg_count += 1
+#
+#
+# def run():
+#     client = connect_mqtt()
+#     # client.loop_start()
+#     publish(client)
+#
+#
+# if __name__ == '__main__':
+#     run()
 
 try:
     GPIO.setmode(GPIO.BCM)  # set GPIO pin mode to BCM numbering
