5487348096d22fce0afe3ed794e3e509efa414f0
Cla Tschenett <cla@aimara.ch>
2023-01-18 16:36:11 +0100

    fix

--- App/checkweight2.py
+++ App/checkweight2.py
@@ -59,10 +59,50 @@ try:
     else:
         print("Tare failed")
     # get the weight
-    weight = hx.get_weight_mean(5)
     mqtt_client = connect_mqtt()
 
+    # -----------------------------
+
+    reading = hx.get_raw_data_mean()
+    if reading:  # always check if you get correct value or only False
+        # now the value is close to 0
+        print('Data subtracted by offset but still not converted to units:',
+              reading)
+    else:
+        print('invalid data', reading)
+
+    # In order to calculate the conversion ratio to some units, in my case I want grams,
+    # you must have known weight.
+    input('Put known weight on the scale and then press Enter')
+    reading = hx.get_data_mean()
+    if reading:
+        print('Mean value from HX711 subtracted by offset:', reading)
+        known_weight_grams = input(
+            'Write how many grams it was and press Enter: ')
+        try:
+            value = float(known_weight_grams)
+            print(value, 'grams')
+        except ValueError:
+            print('Expected integer or float and I have got:',
+                  known_weight_grams)
+
+        # set scale ratio for particular channel and gain which is
+        # used to calculate the conversion to units. Required argument is only
+        # scale ratio. Without arguments 'channel' and 'gain_A' it sets
+        # the ratio for current channel and gain.
+        ratio = reading / value  # calculate the ratio for channel A and gain 128
+        hx.set_scale_ratio(ratio)  # set ratio for current channel
+        print('Ratio is set.')
+    else:
+        raise ValueError('Cannot calculate mean value. Try debug mode. Variable reading:', reading)
+
+
+
+    # -----------------------------
+
+
     while True:
+        weight = hx.get_weight_mean(20)
         if weight:
             print(f"Weight: {weight}")
             # connect to MQTT broker
