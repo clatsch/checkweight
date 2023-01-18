3dfb873fe8330418b77fb3484623887eb44b21aa
Cla Tschenett <cla@aimara.ch>
2023-01-18 21:52:13 +0100

    maxWeight to fload

--- App/checkweight2.py
+++ App/checkweight2.py
@@ -148,7 +148,7 @@ try:
     while True:
         # subscribe(mqtt_client)
         mqtt_client.loop_start()
-        weight = hx.get_weight_mean(5)
+        weight = hx.get_weight_mean(20)
         print(f"Weight: {weight}")
         if weight > maxWeight:
             publish_weight(mqtt_client, weight)
