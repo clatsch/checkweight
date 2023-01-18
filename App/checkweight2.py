a2e6d1dc3824d7f9fe1aae84f47574991f3de535
Cla Tschenett <cla@aimara.ch>
2023-01-18 17:28:03 +0100

    subscribe added

--- App/checkweight2.py
+++ App/checkweight2.py
@@ -87,7 +87,7 @@ try:
     reading = hx.get_data_mean()
     if reading:
         print('Mean value from HX711 subtracted by offset:', reading)
-        known_weight_grams = mqtt_client
+        known_weight_grams = mqtt_client.loop_start()
         try:
             value = float(known_weight_grams)
             print(value, 'grams')
